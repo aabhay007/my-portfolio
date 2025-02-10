416becea2e8e12281d819811bd7552054e1570f4
aabhay007 <abhay007official@gmail.com>
2025-02-10 15:59:16 +0530

    `Added meta data`

--- src/app.vue
+++ src/app.vue
@@ -6,6 +6,18 @@
 
 <script setup>
 import 'aos/dist/aos.css'
+import { useHead } from "#imports";
+
+useHead({
+  title: "My Portfolio",
+  meta: [
+    { name: "description", content: "Welcome to my portfolio site built with Nuxt 3!" },
+    { name: "keywords", content: "Nuxt 3, Vue, Portfolio, Software Developer" },
+    { property: "og:title", content: "My Portfolio" },
+    { property: "og:description", content: "Check out my work and projects." },
+    { property: "og:image", content: "images/profile.jpg" },
+  ],
+});
 </script>
 <style>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.3.0/remixicon.css' );
